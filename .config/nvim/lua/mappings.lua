ec51323cafed0e05af79ab154edd71154849eab1
yongjia <yongjia@YongjiaMac.local>
2024-06-20 22:54:33 +0100

    2024062023

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,23 +4,9 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+vim.keymap.set("n", "<space>fc", ":Telescope file_browser path=%:p:h select_buffer=true<CR>")
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-
-local M = {}
-
-M.lazygit = {
-  n = {
-    ["<leader>gg"] = {
-      function()
-        vim.cmd("silent !lazygit")
-        vim.cmd("redraw!")
-      end,
-      "Open lazygit"
-    },
-  },
-}
-
-return M
